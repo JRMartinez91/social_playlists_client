f7f25ed29e619764ac8970bee2df80591a76c4ca
James Martinez <airtorque1@gmail.com>
2020-08-08 08:56:14 -0400

    Updates

---
+++ precache-manifest.89a5a264aad57eeb10b5aee707087f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28215c800a9799657dc0c51376abe346",
+    "url": "/social_playlists_client/index.html"
+  },
+  {
+    "revision": "14d2632bba18b75b25b7",
+    "url": "/social_playlists_client/static/css/main.dd2c42be.chunk.css"
+  },
+  {
+    "revision": "4c3d954e2641ec55efe2",
+    "url": "/social_playlists_client/static/js/2.c8a03789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_playlists_client/static/js/2.c8a03789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d2632bba18b75b25b7",
+    "url": "/social_playlists_client/static/js/main.6da52e3d.chunk.js"
+  },
+  {
+    "revision": "f74262f80e6411ea1eb0",
+    "url": "/social_playlists_client/static/js/runtime-main.32fc156c.js"
+  }
+]);
